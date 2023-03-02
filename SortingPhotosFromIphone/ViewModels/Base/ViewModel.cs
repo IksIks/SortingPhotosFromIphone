a4083ce1a4c0efecd690a735bdd0d2473b354a36
iksiks30 <iksiks30@gmail.com>
2023-03-02 13:59:18 +0300

    Слияние двух веток

--- SortingPhotosFromIphone/ViewModels/Base/ViewModel.cs
+++ SortingPhotosFromIphone/ViewModels/Base/ViewModel.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SortingPhotosFromIphone.ViewModels.Base
 {
-    internal class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual bool Set<T>(ref T field, T value, [CallerMemberName] string proprtyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(proprtyName);
+            return true;
+        }
+
+
+
+
+
     }
 }
